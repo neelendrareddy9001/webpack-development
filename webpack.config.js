c140e41fa72c34e7e1f1319cb7e85e47ec002001
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-27 22:50:20 +0200

    Added

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path')
 
 module.exports  = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
     output : {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
